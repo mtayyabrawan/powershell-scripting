50bc9c9bd6f102f3c854ab6e39b9c89c05de2969
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-12-10 12:21:29 +0500

    arrays and hashtables

---
+++ 03-arrays-hash-tables.ps1
@@ -0,0 +1,31 @@
+# Arrays
+
+$FavouriteLangs = @("JavaScript", "Cpp", "Pyhton", "PHP")
+
+$FavouriteLangs
+
+$FavouriteLangs[1]
+
+$FavouriteLangs[1].Length
+
+$FavouriteLangs += "C"
+
+$FavouriteLangs
+
+# Hash Tables
+
+$User1 = @{FirstName = "Muhammad"; LastName = "Tayyab"; IsVerified = $true }
+
+$User1
+
+$User1.FirstName
+
+$User1["LastName"]
+
+$User1.Add("Age", 20)
+
+$User1
+
+$User1.Remove("IsVerified")
+
+$User1
