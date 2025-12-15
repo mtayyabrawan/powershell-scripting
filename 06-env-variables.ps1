20cc9f73914c901dd7a3dc722a8f632c58a6a5ca
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-12-15 10:48:35 +0500

    env variables in ps

---
+++ 06-env-variables.ps1
@@ -0,0 +1,7 @@
+# List all envireonment variables
+
+# setting environment variable
+$env:NewEnvVariableSetting = "Muhammad tayyab"
+
+# getting specific environment variable
+$env:NewEnvVariableSetting
