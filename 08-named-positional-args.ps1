a9daeffab9ec7485a9ca0f0ac3581b44b8bb360f
mtayyabrawan <mtayyabawan1918@gmail.com>
2026-03-19 13:16:58 +0500

    named positional arguments

---
+++ 08-named-positional-args.ps1
@@ -0,0 +1,9 @@
+Param([parameter(Position = 0)]$parm1,
+    [parameter(Position = 1)]$parm2,
+    [parameter(Position = 2)]$parm3,
+    [parameter(Position = 3)]$parm4)
+
+Write-Host "Parameter 1: $parm1"
+Write-Host "Parameter 2: $parm2"
+Write-Host "Parameter 3: $parm3"
+Write-Host "Parameter 4: $parm4"
