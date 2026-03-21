a117af56e801ace95e23d74dcd25926c966317cc
mtayyabrawan <mtayyabawan1918@gmail.com>
2026-03-21 20:16:58 +0500

    updated gcp project

--- projects/gcp.ps1
+++ projects/gcp.ps1
@@ -5,12 +5,25 @@ $CommitTime = $args[1]
 
 $ParsedTime = $CommitTime.Split(" ")
 
+$LastCommitDate = git log -1 --format='%cI'
+
+$days = [int]($ParsedTime[0] ?? 0)
+$hours = [int]($ParsedTime[1] ?? 0)
+$minutes = [int]($ParsedTime[2] ?? 0)
+$seconds = [int]($ParsedTime[3] ?? 0)
+
 # current commit time based on last commit
-$CurrentDate = (Get-Date -Date "$(git log -1 --format='%ci')").AddDays($ParsedTime[0]).AddHours($ParsedTime[1]).AddMinutes($ParsedTime[2]).AddSeconds($ParsedTime[3])
+$CurrentDate = (Get-Date $LastCommitDate).
+    AddDays($days).
+    AddHours($hours).
+    AddMinutes($minutes).
+    AddSeconds($seconds)
+
+$FormattedDate = $CurrentDate.ToString("yyyy-MM-ddTHH:mm:sszzz")
 
 # git time envireonment variables
-$env:GIT_COMMITTER_DATE = $CurrentDate
-$env:GIT_AUTHOR_DATE = $CurrentDate
+$env:GIT_COMMITTER_DATE = $FormattedDate
+$env:GIT_AUTHOR_DATE = $FormattedDate
 
 # commiting to git
 git commit -m "$CommitMessage"
